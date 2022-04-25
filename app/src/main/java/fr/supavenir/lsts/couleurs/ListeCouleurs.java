d941e590a46c11ae7625e9d39ca7fc11c4b44877
MartinJeanne <martinjeanne.dev@gmail.com>
2022-04-25 11:45:31 +0200

    Don't automatically populate DB

--- app/src/main/java/fr/supavenir/lsts/couleurs/ListeCouleurs.java
+++ app/src/main/java/fr/supavenir/lsts/couleurs/ListeCouleurs.java
@@ -66,7 +66,7 @@ public class ListeCouleurs extends AppCompatActivity {
 
         boolean dbUpToDate = checkDbState();
         if(!dbUpToDate) {
-            createAndPopulateDb();
+            //createAndPopulateDb();
             writeDbState();
         }
 
